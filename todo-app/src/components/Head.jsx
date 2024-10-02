26fe96ec43e9a7ff34c8f7703b62c6d330a5a82f
LEE WOOJIN <24864@naver.com>
2024-10-02 15:25:02 +0900

    기초세팅

---
+++ todo-app/src/components/Head.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Head(props) {
+    return (
+        <div id='header'>
+            <header className='header'>
+                <h2>Todo List</h2>
+            </header>
+        </div>
+    );
+}
+
+export default Head;
